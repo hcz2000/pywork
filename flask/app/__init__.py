0340e81d47ad377e804e5fcc00e442624005dbcd
hcz2000 <huangchangzhan@hotmail.com>
2018-06-22 22:48:19 +0800

    update

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,12 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 app=Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+mysqlconnector://root:@localhost:3306/test'
-#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['FLASKY_ADMIN']='huangchangzhan@hotmail.com'
-#app.config['SECRECT_KEY']='hard to guess string'
- 
+
 db = SQLAlchemy()
 
 def create_app(config_name):
