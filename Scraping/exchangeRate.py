a591e7670a4707e869efea15dc4e30fdf220edc8
hcz2000 <huangchangzhan@hotmail.com>
2019-03-30 23:45:43 +0800

    update

--- Scraping/exchangeRate.py
+++ Scraping/exchangeRate.py
@@ -1,54 +1,64 @@
-from urllib import request,parse
+from urllib import request, parse
 from html.parser import HTMLParser
 
+
 class RatesHTMLParser(HTMLParser):
+    tags = ['div', 'table', 'tr', 'td', None]
+    keys = ['class', None, None, None]
+    values = ['BOC_main publish', None, None, None]
+    matches = [False, False, False, False]
+
     def __init__(self):
         HTMLParser.__init__(self)
-        self.tags=['div','table','tr','td']
-        self.level=0
-        self.item=[]
-        self.items=[]
-      
-    def handle_starttag(self,tag,attrs):
-        if self.level<len(self.tags) and tag==self.tags[self.level]:
-            if self.level==0:
-                for key,value in attrs:
-                    if key=='class' and value=='BOC_main publish':
-                        self.level=self.level+1
+        self.__level = 0
+        self.__item = []
+        self.__items = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == self.tags[self.__level]:
+            if self.keys[self.__level] == None:
+                self.matches[self.__level] = True
             else:
-                self.level=self.level+1;
-      
-                
-    def handle_data(self,data):
-        if self.level==4:
-            self.item.append(data)
+                for key, value in attrs:
+                    if key == self.keys[self.__level] and value == self.values[self.__level]:
+                        self.matches[self.__level] = True
+        if self.matches[0]:
+            self.__level = self.__level + 1
 
-    def handle_endtag(self,tag):
-        if self.level>0 and tag==self.tags[self.level-1]:
-            if self.level==3:
-                if len(self.item)>0:
-                    self.items.append(self.item)
-                    self.item=[]
-            self.level=self.level-1
-        
+    def handle_data(self, data):
+        if all(self.matches):
+            self.__item.append(data)
+
+    def handle_endtag(self, tag):
+        if self.matches[0]:
+            self.__level = self.__level - 1
+            if self.matches[-1] == False and len(self.__item) > 0:
+                self.__items.append(self.__item)
+                self.__item = []
+            self.matches[self.__level] = False
+
     def getItems(self):
-        return self.items
+        return self.__items
 
 
-def getExchangeRate(): 
-    url= 'http://srh.bankofchina.com/search/whpj/search.jsp'
+def getExchangeRate():
+    url = 'http://srh.bankofchina.com/search/whpj/search.jsp'
 
-    parms={'erectDate':'21','nothing':'2016-07-12','pjname':'1316'}
-    querystring=parse.urlencode(parms)
-   
-    req=request.urlopen(url,querystring.encode('ascii'))
-    resp=req.read().decode('utf-8')
+    parms = {'erectDate': '2019-03-01', 'nothing': '2019-03-10', 'pjname': '1316'}
+    querystring = parse.urlencode(parms)
 
-    parser=RatesHTMLParser()
+    req = request.urlopen(url, querystring.encode('ascii'))
+    resp = req.read().decode('utf-8')
+
+    # print(resp)
+    parser = RatesHTMLParser()
     parser.feed(resp)
     parser.close()
     return parser.getItems()
 
-if __name__== '__main__':
-    getExchangeRate()
+
+if __name__ == '__main__':
+    items = getExchangeRate()
+    for item in items:
+        print(item)
 
