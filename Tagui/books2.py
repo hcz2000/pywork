eed957c24a27782c338d9b0d419e2edbd086af8c
hcz <huangchangzhan@hotmail.com>
2023-07-24 18:35:11 +0800

    update

--- Tagui/books2.py
+++ Tagui/books2.py
@@ -1,6 +1,7 @@
 from lxml import etree
 import urllib.request
 import os
+import time
 
 class BookInfo():
 
@@ -8,27 +9,27 @@ class BookInfo():
         self.basePath = os.path.dirname(__file__)
 
     def getInfo(self,bookno):
-        url="https://m.23sk.net/files/article/html/"+bookno[:-len(bookno)+3]+"/"+bookno+"/"
-        print(url)
-
+        url="https://m.23sk.net/files/article/html/"+bookno[:len(bookno)-3]+"/"+bookno+"/"
+        #print(url)
         response = urllib.request.urlopen(url)
         content=response.read().decode('utf-8')
-        #print(content)
 
         html=etree.HTML(content)
 
-        names = html.xpath("//div[@class='block_txt2']/p")
-        print(names)
-        #elements= tree.xpath("//div[@class='block_txt2']/p")
-        #author = elements[2]
-        #print(author.text)
-        #type = elements[3]
-        #print(type.text)
-        #update = elements[5]
-        #print(update.text)
+        elements = html.xpath("//div[@class='block_txt2']/p")
+        name = elements[0].findtext("a/h2")
+
+        author = elements[1].text[3:]
+        type = elements[2].findtext("a")
+        update = elements[4].text[3:]
         info = html.xpath("//div[@class='intro_info']/text()")[0]
         #print(info)
+        print(bookno+','+name+','+author+','+type+','+update)
+
 
 if __name__ == '__main__':
-    obj = BookInfo()
-    obj.getInfo('127418')
+    store = BookInfo()
+    #store.getInfo('127418')
+    for no in range(10000,999999):
+        store.getInfo(str(no))
+        time.sleep(1)
