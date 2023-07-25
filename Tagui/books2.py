11d16dcf4251409c3738bb18eac2c55c51367555
hcz <huangchangzhan@hotmail.com>
2023-07-25 19:29:03 +0800

    update

--- Tagui/books2.py
+++ Tagui/books2.py
@@ -1,16 +1,39 @@
 from lxml import etree
 import urllib.request
-import os
 import time
+import pymysql
 
 class BookInfo():
 
-    def __init__(self):
-        self.basePath = os.path.dirname(__file__)
+    def __init__(self,database,user,password,host,port):
+        self.conn=pymysql.connect(database=database, user=user, password=password, host=host, port=port)
+
+    def __del__(self):
+        self.conn.close()
+
+    def queryDB(self,sql):
+        try:
+            cur=self.conn.cursor()
+            cur.execute(sql)
+            result=cur.fetchall()
+            cur.close()
+            return result
+        except Exception as e:
+            print(e)
+
+    def updateDB(self,sql):
+        try:
+            cur=self.conn.cursor()
+            cur.execute(sql)
+            self.conn.commit()
+            cur.close()
+            return result
+        except Exception as e:
+            print(e)
 
     def getInfo(self,bookno):
         url="https://m.23sk.net/files/article/html/"+bookno[:len(bookno)-3]+"/"+bookno+"/"
-        #print(url)
+        print(url)
         response = urllib.request.urlopen(url)
         content=response.read().decode('utf-8')
 
@@ -25,11 +48,12 @@ class BookInfo():
         info = html.xpath("//div[@class='intro_info']/text()")[0]
         #print(info)
         print(bookno+','+name+','+author+','+type+','+update)
+        sql="insert into book values('"+bookno+"','"+name+"','"+author+"','"+info+"')"
+        self.updateDB(sql)
 
 
 if __name__ == '__main__':
-    store = BookInfo()
-    #store.getInfo('127418')
-    for no in range(10000,999999):
+    store = BookInfo("book", "root", "root", "localhost", 3306)
+    for no in range(100000,999999):
         store.getInfo(str(no))
         time.sleep(1)
