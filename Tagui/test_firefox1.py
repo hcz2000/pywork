4e4feade0a53e490f0c76dd95b00bfe196e9d697
hcz <huangchangzhan@hotmail.com>
2024-05-15 17:54:08 +0800

    update

---
+++ Tagui/test_firefox1.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+import yaml
+
+class BocwmValue():
+
+    def __init__(self):
+        with open('wm.yaml', 'r') as file:
+            config=yaml.safe_load(file)
+        self.products = config['bocwm']['products']
+        self.basePath = os.path.dirname(__file__)
+
+    def getInfo(self,url):
+        with webdriver.Firefox() as driver:
+            driver.implicitly_wait(30)
+            driver.get(url)
+            contentDiv=driver.find_element(By.ID,'content')
+            rows = contentDiv.find_elements(By.XPATH,"//table[@class='layui-table']/tbody/tr")
+            for row in rows:
+                cols=row.find_elements(By.TAG_NAME,'td')
+                print(cols[0].text,cols[1].text,cols[2].text,cols[6].text)
+
+    def refresh(self):
+        for product in self.products:
+            #(code,url)=url_str.split(':')
+            code=product['code']
+            url=product['url']
+            print(code,url)
+            self.getInfo(url)
+
+if __name__ == '__main__':
+    obj = BocwmValue()
+    obj.refresh()
