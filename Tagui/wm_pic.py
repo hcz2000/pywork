0d70aacb93158ca632011cb0dd8344beb9ed458d
hcz <huangchangzhan@hotmail.com>
2024-05-24 18:02:23 +0800

    update

--- Tagui/wm_pic.py
+++ Tagui/wm_pic.py
@@ -29,19 +29,21 @@ class WmPic():
             print(code)
             fname='./data/%s.csv' % code
             dt=[('date','U16'),('netvalue','f4')]
-            #net_values=np.loadtxt(fname,converters={0:date_converter},delimiter=',')
-            net_values = np.loadtxt(fname,  dt, delimiter=',')
-            xdata=net_values['date'][-16:]
-            ydata=net_values['netvalue'][-16:]
+            net_values=np.loadtxt(fname,converters={0:date_converter},delimiter=',')
+            xdata = net_values[:,0][-16:]
+            ydata = net_values[:,1][-16:]
+            #net_values = np.loadtxt(fname,  dt, delimiter=',')
+            #xdata=net_values['date'][-16:]
+            #ydata=net_values['netvalue'][-16:]
             line, = ax.plot(xdata,ydata, lw=1, label=code)
             for label in ax.get_xticklabels():
                 label.set_rotation(90)
-            ax.xaxis.set_major_locator(dates.DateLocator(interval=1))
-            ax.xaxis.set_major_formatter(dates.DateFromatter('%Y-%m-%d'))
+            ax.xaxis.set_major_locator(dates.DateLocator())
+            ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
             font={'family':'SimHei','weight':'normal','size':10}
-            ax.set_ylabel('netvalue',font)
-            ax.set_xlabel('date',font)
-            ax.set_title('理财产品净值')
+            ax.set_ylabel('净值',font)
+            ax.set_xlabel('时间',font)
+            ax.set_title('理财产品净值',font)
             plt.show()
             break
 
