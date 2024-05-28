3b73a1f908382b38038352dc611d0a114228cb1f
hcz <huangchangzhan@hotmail.com>
2024-05-28 18:18:07 +0800

    update

--- Tagui/wm_pic.py
+++ Tagui/wm_pic.py
@@ -25,7 +25,8 @@ class WmPic():
         ax = fig.add_subplot(111)
         for product in self.products:
             code = product['code']
-            print(code)
+            desc = product['desc']
+            print(code,desc)
             fname = './data/%s.csv' % code
             #net_values = np.loadtxt(fname, converters={0: (lambda s: np.datetime64(s))}, delimiter=',')
             #xdata = net_values[:, 0]
@@ -34,19 +35,17 @@ class WmPic():
             net_values = np.loadtxt(fname,  dt, delimiter=',')
             xdata=[datetime.strptime(date,'%Y-%m-%d').date() for date in net_values['date']]
             ydata=net_values['netvalue']
-            print(xdata,ydata)
-            line, = ax.plot(xdata, ydata, lw=2, label=code)
-            #line.set_visible(False)
+            line, = ax.plot(xdata, ydata, lw=4, label=desc)
             for label in ax.get_xticklabels():
                 label.set_rotation(60)
             ax.xaxis.set_major_locator(dates.MonthLocator())
             ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m'))
             ax.xaxis.set_minor_locator(dates.DayLocator(interval=1))
-            font = {'family': 'SimHei', 'weight': 'normal', 'size': 10}
+            font = {'family': 'SimHei', 'weight': 'normal', 'size': 18}
             ax.set_ylabel('净值', font)
             ax.set_xlabel('时间', font)
             ax.set_title('理财产品净值', font)
-        plt.legend()
+        plt.legend(prop={'family': 'SimHei', 'size': 16})
         plt.show()
 
 
