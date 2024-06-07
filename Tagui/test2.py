db471cb1fe380f2bca8128ecb2f3c4233504c35e
hcz <huangchangzhan@hotmail.com>
2024-06-07 17:04:33 +0800

    update

--- Tagui/test2.py
+++ Tagui/test2.py
@@ -5,4 +5,13 @@ checkbox_var=tk.IntVar()
 checkbox=tk.checkbutton(root,text='中银',variable=checkbox_var)
 
 def button_click():
-    checkbox_var
+    checkbox_value=checkbox_var.get()
+    print(checkbox_value)
+
+
+button=tk.Button(root,text='确定',command=button_click)
+label=tk.Label(root,text='label')
+checkbox.pack()
+button.pack()
+label.pack()
+root.mainloop()
