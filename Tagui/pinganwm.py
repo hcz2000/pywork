59cf6afef463d406edb1c98c457b19983d879e42
hcz <huangchangzhan@hotmail.com>
2024-06-28 21:24:03 +0800

    update

--- Tagui/pinganwm.py
+++ Tagui/pinganwm.py
@@ -64,8 +64,11 @@ class Pinganwmvalue(WmValue):
     def getNetValue(self, product):
         code = product['code']
         url = product['url']
+        value_type=product['value_type']
         net_values = []
-        last_sync_date = self.getLastRecord(code)[0]
+        last_record=self.getLastRecord(code)
+        last_sync_date = last_record[0]
+        last_net_value = float(last_record[1])
         self.driver.implicitly_wait(30)
         self.driver.get(url)
 
@@ -76,17 +79,17 @@ class Pinganwmvalue(WmValue):
         tbody = self.driver.find_element(By.XPATH, "//div[@class='ant-table-body']/table/tbody")
         outputList = tbody.find_elements(By.TAG_NAME, 'tr')
         newest_report = outputList[0]
-        newest_release_date = newest_report.find_elements(By.TAG_NAME, 'td')[-1].text
+        newest_release_date = newest_report.find_elements(By.TAG_NAME, 'td')[1].text
         if newest_release_date <= last_sync_date:
             print('No new release:', last_sync_date)
             return
 
         while True:
             oldest_report = outputList[-1]
-            oldest_release_date = oldest_report.find_elements(By.TAG_NAME, 'td')[-1].text
+            oldest_release_date = oldest_report.find_elements(By.TAG_NAME, 'td')[1].text
             if oldest_release_date >= last_sync_date:
                 pagediv = self.driver.find_element(By.XPATH, "//div[@class='ant-spin-container']")
-                next_page = pagediv.find_element(By.XPATH, "//li[@title='下一页']/a").text
+                next_page = pagediv.find_element(By.XPATH, "//li[@title='下一页']")
                 if next_page.get_attribute('aria-disabled') == 'true':
                     break
                 else:
@@ -101,34 +104,52 @@ class Pinganwmvalue(WmValue):
             else:
                 break
 
+        accumulated_revenue = last_net_value - 1.00
         while True:
             reversed_list = outputList[::-1]
             for row in reversed_list:
                 cols = row.find_elements(By.TAG_NAME, 'td')
-                release_date = cols[-1].text
+                release_date = cols[1].text
                 if release_date < last_sync_date:
                     break
 
                 time.sleep(1)
 
-                rpt_date = cols[1].text
-                net_value = cols[2].text
-                if rpt_date > last_sync_date:
-                    net_values.append((rpt_date, net_value))
+                if value_type == 'net_value':
+                    rpt_date = cols[1].text
+                    net_value = cols[2].text
+                    print(rpt_date,net_value)
+                    if rpt_date > last_sync_date:
+                        net_values.append((rpt_date, net_value))
+                else:
+                    revenues = []
+                    for row in reversed_list:
+                        cols = row.find_elements(By.TAG_NAME, 'td')
+                        (rpt_date, revenue) = (cols[1].text, float(cols[2].text) / 10000)
+                        if rpt_date > last_sync_date:
+                            revenues.append((rpt_date, revenue))
+                        else:
+                            break
 
-                self.driver.close()
-                self.driver.switch_to.window(self.driver.window_handles[0])
+                    for row in revenues[::-1]:
+                        accumulated_revenue = accumulated_revenue + (1.0 + accumulated_revenue) * row[1]
+                        (rpt_date, net_value) = (row[0], 1.0 + accumulated_revenue)
+                        print(rpt_date, net_value)
+                        net_values.append((rpt_date, net_value))
 
             pagediv = self.driver.find_element(By.XPATH, "//div[@class='ant-spin-container']")
-            prev_page = pagediv.find_element(By.XPATH, "//li[@title='上一页']/a").text
-            prev_link = prev_page.find_element(By.TAG_NAME, "a")
-            if prev_link:
-                prev_link.click()
-                time.sleep(2)
-                tbody = self.driver.find_element(By.XPATH, "//div[@class='ant-table-body']/table/tbody")
-                outputList = tbody.find_elements(By.TAG_NAME, 'tr')
-            else:
+            prev_page = pagediv.find_element(By.XPATH, "//li[@title='上一页']")
+            if prev_page.get_attribute('aria-disabled') == 'true':
                 break
+            else:
+                prev_link = prev_page.find_element(By.TAG_NAME, "a")
+                if prev_link:
+                    prev_link.click()
+                    time.sleep(2)
+                    tbody = self.driver.find_element(By.XPATH, "//div[@class='ant-table-body']/table/tbody")
+                    outputList = tbody.find_elements(By.TAG_NAME, 'tr')
+                else:
+                    break
 
         self.write2CsvFile(code, net_values)
 
