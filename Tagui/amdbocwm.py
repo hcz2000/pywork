a442adbb003e9819439e143460576e66125e579d
hcz2000 <huangchangzhan@hotmail.com>
2024-07-28 14:02:50 +0800

    update

--- Tagui/amdbocwm.py
+++ Tagui/amdbocwm.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import urllib
 import pdfplumber
 import os
@@ -67,24 +69,21 @@ class Amdbocwmvalue(WmValue):
         net_values=[]
         last_sync_date = self.getLastRecord(code)[0]
         self.driver.implicitly_wait(10)
-        print(datetime.now(),'访问',url)
         self.driver.get(url)
+        wait=WebDriverWait(driver,10)
         print(datetime.now(),'get(url)返回')
-
         report_type=self.driver.find_element(By.XPATH, "//dl[@id='bglxspan_box']/dd")
-        print(datetime.now(),'0001')
         report_type.find_element(By.LINK_TEXT,'净值报告').click()
-        time.sleep(5)
-        print(datetime.now(),'0002')
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='zzpl_search']/div[1]/ul/li[last()]/input")))
+        #time.sleep(5)
         search_input = self.driver.find_element(By.XPATH, "//div[@class='zzpl_search']/div[1]/ul/li[last()]/input")
         search_input.clear()
         search_input.send_keys(code)
-        print(datetime.now(),'0003')
         search_button = self.driver.find_element(By.XPATH,"//div[@class='zzpl_search']/div[last()]/a[1]")
         search_button.click()
-        time.sleep(2)
-        print(datetime.now(),'0004')
-
+        wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@id='pro_list']/table/tbody[last()]")))
+        #time.sleep(2)
         tbody=self.driver.find_element(By.XPATH, "//div[@id='pro_list']/table/tbody[last()]")
         outputList = tbody.find_elements(By.TAG_NAME, 'tr')
         newest_report = outputList[0]
